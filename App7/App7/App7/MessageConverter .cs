f7c9639fd58a72d4f32337623dd5bb33027ecc24
Pradhap Palani <pradhap.palani@syncfusion.com>
2020-12-18 21:01:56 +0530

    Added samples for How-to-convert-data-objects-to-messages-in-MVVM-in-Xamarin.Forms-chat-SfChat.

---
+++ App7/App7/App7/MessageConverter .cs
@@ -0,0 +1,54 @@
+﻿using Syncfusion.XForms.Chat;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App7
+{
+    /// <summary>
+    /// Defines methods that can be used to convert data objects to chat messages and vice versa.
+    /// </summary>
+    public class MessageConverter : IChatMessageConverter
+    {
+        /// <summary>
+        /// Converts given data object to a chat message.
+        /// </summary>
+        /// <param name="data">The data object to be converted as a chat message.</param>
+        /// <param name="chat">Instance of <see cref="SfChat"/>. </param>
+        /// <returns>Returns the data object as a chat message.</returns>
+        public IMessage ConvertToChatMessage(object data, SfChat chat)
+        {
+            var message = new TextMessage();
+            var item = data as MessageModel;
+
+            message.Text = item.Text;
+            message.Author = item.User;
+            message.Data = item;
+            if (item.Suggestions != null)
+            {
+                message.Suggestions = item.Suggestions;
+            }
+            return message;
+        }
+
+        /// <summary>
+        /// Converts the given chat message to a data object.
+        /// </summary>
+        /// <param name="chatMessage">The chat message that is to be converted as a data object.</param>
+        /// <param name="chat">Instance of <see cref="SfChat"/>. </param>
+        /// <returns>Returns the chat message as a data object.</returns>
+        public object ConvertToData(object chatMessage, SfChat chat)
+        {
+            var message = new MessageModel();
+            var item = chatMessage as TextMessage;
+
+            message.Text = item.Text;
+            message.User = chat.CurrentUser;
+            if (message.Suggestions != null)
+            {
+                message.Suggestions = chat.Suggestions;
+            }
+            return message;
+        }
+    }
+}
---
+++ App7/App7/App7/ViewModel/GettingStartedViewModel.cs
@@ -0,0 +1,99 @@
+﻿using App7;
+using Syncfusion.XForms.Chat;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace App7
+{
+    public class GettingStartedViewModel : INotifyPropertyChanged
+    {         
+        /// <summary>
+        /// Current user of chat.
+        /// </summary>
+        private Author currentAuthor;
+
+        ObservableCollection<MessageModel> messageCollection;
+
+        public GettingStartedViewModel()
+        {
+            MessageModel messageModel = new MessageModel();          
+            this.messageCollection = new ObservableCollection<MessageModel>();        
+            this.currentAuthor = new Author() { Name = "Nancy", Avatar = "People_Circle2.png" };
+            this.GenerateMessages();
+        }
+       
+        /// <summary>
+        /// Gets or sets the collection of messages of a conversation.
+        /// </summary>
+        public ObservableCollection<MessageModel> MessageCollection
+        {
+            get
+            {
+                return this.messageCollection;
+            }
+
+            set
+            {
+                this.messageCollection = value;
+                RaisePropertyChanged("messageCollection");
+            }
+        }       
+
+        /// <summary>
+        /// Gets or sets the current user of the message.
+        /// </summary>
+        public Author CurrentUser
+        {
+            get
+            {
+                return this.currentAuthor;
+            }
+            set
+            {
+                this.currentAuthor = value;
+                RaisePropertyChanged("CurrentUser");
+            }
+        }
+
+        /// <summary>
+        /// Property changed handler.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Occurs when property is changed.
+        /// </summary>
+        /// <param name="propName">changed property name</param>
+        public void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private void GenerateMessages()
+        {           
+            this.messageCollection.Add(new MessageModel()
+            {
+                User = currentAuthor,
+                Text = "Hi guys, good morning! I'm very delighted to share with you the news that our team is going to launch a new mobile application.",             
+            });
+
+            this.messageCollection.Add(new MessageModel()
+            {
+                User = new Author() { Name = "Andrea", Avatar = "People_Circle7.png" },
+                Text = "Oh! That's great.",
+            });
+
+            this.messageCollection.Add(new MessageModel()
+            {
+                User = new Author() { Name = "Harrison", Avatar = "People_Circle5.png" },
+                Text = "That is good news.",              
+            });           
+        }
+    }
+}
